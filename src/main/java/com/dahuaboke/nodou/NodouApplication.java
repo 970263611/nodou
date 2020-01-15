59d44b7ff90dec4ac2ad1ff919984b2f6160f928
dingwq <dingwq@i2finance.net>
2020-01-15 08:53:02 +0800

    提交nodou项目

---
+++ src/main/java/com/dahuaboke/nodou/NodouApplication.java
@@ -0,0 +1,13 @@
+package com.dahuaboke.nodou;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NodouApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NodouApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dahuaboke/nodou/NodouApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dahuaboke.nodou;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NodouApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
