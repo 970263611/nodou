9ee903337c20f31de6ef54ce1459f0f46df74497
dingwq <dingwq@i2finance.net>
2020-01-19 10:57:30 +0800

    nodou 1.0 初步完成

--- src/main/java/com/dahuaboke/nodou/manager/TaskManager.java
+++ src/main/java/com/dahuaboke/nodou/manager/TaskManager.java
@@ -1,6 +1,8 @@
 package com.dahuaboke.nodou.manager;
 
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.alibaba.fastjson.JSONObject;
 import com.dahuaboke.nodou.model.NodeModel;
 import com.dahuaboke.nodou.timeTask.HeartbeatTask;
 import com.dahuaboke.nodou.timeTask.PersistenceTask;
@@ -9,6 +11,8 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -21,20 +25,28 @@ public class TaskManager {
     @Bean
     public PersistenceTask persistenceTask() {
         String jsonString = FileUtil.read(log_address);
-        if(jsonString != null && !"".equals(jsonString)){
-//            List<Map> list = JSON.parseArray(jsonString, Map.class);
-//            for(Map map : list){
-//                NodeManager.parentNode.putAll(map);
-//            }
-            NodeManager.parentNode = new NodeModel<String, NodeModel>();
-        }else{
-            NodeManager.parentNode = new NodeModel<String, NodeModel>();
+        NodeManager.parentNode = new NodeModel<String, NodeModel>();
+        if (jsonString != null && !"".equals(jsonString)) {
+            List<Map> list = JSON.parseArray(jsonString, Map.class);
+            for (Map map : list) {
+//                for (Object key : map.keySet()) {
+//                    JSONObject o = (JSONObject) map.get(key);
+//                    NodeModel nodeModel = new NodeModel();
+//                    for(String jsonKey : o.keySet()){
+//                        JSONArray jsonArray = o.getJSONArray(jsonKey);
+//                        List<String> temp = jsonArray.toJavaList(String.class);
+//                        nodeModel.put(jsonKey,temp);
+//                    }
+//                    NodeManager.parentNode.put(key,nodeModel);
+//                }
+                NodeManager.parentNode.putAll(map);
+            }
         }
         return new PersistenceTask(log_address);
     }
 
     @Bean
-    public HeartbeatTask heartbeatTask(){
+    public HeartbeatTask heartbeatTask() {
         return new HeartbeatTask();
     }
 }
