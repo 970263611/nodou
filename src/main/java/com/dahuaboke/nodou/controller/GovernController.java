5cf4e8358f617320f3a80622062baebaa6898195
dingweiqiang <970263611@qq.com>
2020-02-06 13:26:42 +0800

    提交nodou正式版本1.0.0

--- src/main/java/com/dahuaboke/nodou/controller/GovernController.java
+++ src/main/java/com/dahuaboke/nodou/controller/GovernController.java
@@ -1,38 +1,21 @@
 package com.dahuaboke.nodou.controller;
 
-import java.net.Inet4Address;
-import java.net.InetAddress;
-import java.net.NetworkInterface;
-import java.util.Enumeration;
+import com.dahuaboke.nodou.manager.NodeManager;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 public class GovernController {
 
-    public static void main(String[] args) {
-        System.out.println("本机IP:" + getIpAddress());
+    @RequestMapping("/")
+    public String govern(){
+        return "govern";
     }
 
-    public static String getIpAddress() {
-        try {
-            Enumeration<NetworkInterface> allNetInterfaces = NetworkInterface.getNetworkInterfaces();
-            InetAddress ip = null;
-            while (allNetInterfaces.hasMoreElements()) {
-                NetworkInterface netInterface = (NetworkInterface) allNetInterfaces.nextElement();
-                if (netInterface.isLoopback() || netInterface.isVirtual() || !netInterface.isUp()) {
-                    continue;
-                } else {
-                    Enumeration<InetAddress> addresses = netInterface.getInetAddresses();
-                    while (addresses.hasMoreElements()) {
-                        ip = addresses.nextElement();
-                        if (ip != null && ip instanceof Inet4Address) {
-                            return ip.getHostAddress();
-                        }
-                    }
-                }
-            }
-        } catch (Exception e) {
-            System.err.println("IP地址获取失败" + e.toString());
-        }
-        return "";
+    @RequestMapping("/governData")
+    @ResponseBody
+    public Object governData(){
+        return NodeManager.parentNode;
     }
-
 }
